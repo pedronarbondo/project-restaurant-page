5b79c75f4705027062c1b18fa876da17bd54254d
Pedro <pedro.narbondo@hotmail.com>
2022-06-12 16:18:45 -0300

    Add menu tab

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import StylishFont from "./fonts/NightinTokyo.ttf";
 import OtherFont from "./fonts/Manga.ttf";
 
 import loadMain from "./main";
+import loadMenu from "./menu";
+
 
 loadMain();
+loadMenu();
 
+/*const buttons = Array.from(document.querySelectorAll("button"));
+const button1 = buttons[0];
+const button2 = buttons[1];
+const button3 = buttons[3];
 
+button1.addEventListener("click", loadMain);
+button2.addEventListener("click", loadMenu);
+*/
---
+++ src/menu.js
@@ -0,0 +1,48 @@
+import {loadHome} from "./runWebsite";
+import {createStuff} from "./runWebsite";
+import Roll1 from "./img/roll1.png";
+import Roll2 from "./img/roll2.png";
+import Roll3 from "./img/roll3.png";
+import Roll4 from "./img/roll4.png";
+import Roll5 from "./img/roll5.png";
+import Ramen from "./img/ramen.png";
+
+
+const createMenu = function() {
+    const body = document.getElementById("content");
+    const foodContainer = document.createElement("div");
+    foodContainer.classList.add("mainDiv", "foodContainer");
+
+    foodContainer.append(
+        createMenuItem("Roll I", "Orange balls surrounded by an algae", Roll1),
+        createMenuItem("Roll II", "Fish surrounded by rice surrounded by algae", Roll2),
+        createMenuItem("Roll III", "Some veggies surrounded by seeds-covered rice", Roll3),
+        createMenuItem("Roll IV", "Veggies and rice surrounded by a chunk of salmon", Roll4),
+        createMenuItem("Roll V", "Our best seller: veggies, cream cheese, rice and salmon", Roll5),
+        createMenuItem("Ramen I", "This is the stuff of dreams", Ramen)
+    )
+    body.append(foodContainer);
+}
+
+const createMenuItem = function(name, description, src) {
+    const item = document.createElement("div");
+    item.className = "item";
+
+    const itemTitle = createStuff("h3", "foodTitle", name);
+    const itemDescription = createStuff("p", "itemDescription", description);
+    const itemPhoto = new Image();
+    itemPhoto.src = src;
+    itemPhoto.className = "itemPhoto";
+
+    item.append(itemPhoto, itemTitle, itemDescription);
+    return item;
+}
+
+function loadMenu() {
+    loadHome();
+    createMenu();
+}
+
+
+
+export default loadMenu;
