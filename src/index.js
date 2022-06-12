64ece61f8f10db0aa285a5b58a394a978997a17e
Pedro <pedro.narbondo@hotmail.com>
2022-06-12 16:44:02 -0300

    Work on buttons next

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import Sushi from './img/sushi.jpg';
 import StylishFont from "./fonts/NightinTokyo.ttf";
 import OtherFont from "./fonts/Manga.ttf";
 
+
 import loadMain from "./main";
 import loadMenu from "./menu";
 import loadContact from "./contact";
 
 
 loadMain();
-loadMenu();
 
-
-/*const buttons = Array.from(document.querySelectorAll("button"));
-const button1 = buttons[0];
-const button2 = buttons[1];
-const button3 = buttons[3];
-
-button1.addEventListener("click", loadMain);
-button2.addEventListener("click", loadMenu);
-*/
